6d1d67448688d8f67909156f95f0845c3ae6739e
derekmhawkins <derekmhawkins@gmail.com>
2019-01-08 10:14:31 -0600

    created firebase user authentication interface(methods)

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 var prodConfig = {
   apiKey: process.env.REACT_APP_PROD_API_KEY,
@@ -22,6 +23,13 @@ const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 export default class Firebase {
   constructor() {
-    app.initializeApp(config)
+    app.initializeApp(config);
+    this.auth = app.auth();
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+  doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+  doSignOut = () => this.auth.signOut();
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
