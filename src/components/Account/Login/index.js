e67580214f14d82c9318054b1fcaa95489ee23ad
derekmhawkins <derekmhawkins@gmail.com>
2019-01-08 13:53:55 -0600

    registration form functional

--- src/components/Account/Login/index.js
+++ src/components/Account/Login/index.js
@@ -1,4 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { RegistrationLink } from '../Register';
+import { withFirebase } from '../../Firebase';
+
+import * as ROUTES from '../../../constants/routes';
+
+const LoginPage = () => (
+  <div>
+    <h1>Login Page</h1>
+
+    <RegistrationLink />
+  </div>
+)
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null
+}
+
+class LoginFormBase extends Component {
+  constructor() {
+    super()
+    this.state = { ...INITIAL_STATE }
+  }
+
+  onSubmit = e => {
+    const { email, password } = this.state;
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(err => this.setState({ err }));
+      e.preventDefault();
+  }
+
+  // onChange = e =>
+}
 
 export default class Login extends Component {
   render() {
